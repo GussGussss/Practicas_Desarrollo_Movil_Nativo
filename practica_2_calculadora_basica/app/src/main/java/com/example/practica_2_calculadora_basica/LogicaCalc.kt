99facc7e3d2ee155d69104481ba956573559ee9b
Angel Gustavo Pacheco Meza <gustavopacheco3005@gmail.com>
2026-09-13 15:32:52 -0700

    Calculadora a la mitad

--- practica_2_calculadora_basica/app/src/main/java/com/example/practica_2_calculadora_basica/LogicaCalc.kt
+++ practica_2_calculadora_basica/app/src/main/java/com/example/practica_2_calculadora_basica/LogicaCalc.kt
@@ -1,4 +1,13 @@
 package com.example.practica_2_calculadora_basica
 
-class LogicaCalc {
+import androidx.compose.ui.graphics.PathOperation
+
+object LogicaCalc{
+    fun calcular(num1: String, num2: String, operation: String): String{
+        val n1 = num1.toDoubleOrNull() ?: return ""
+        val n2 = num2.toDoubleOrNull() ?: return ""
+        val result = when (operation){
+
+        }
+    }
 }
