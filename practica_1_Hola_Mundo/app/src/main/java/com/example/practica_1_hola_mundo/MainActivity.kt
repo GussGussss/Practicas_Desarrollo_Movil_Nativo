e480d00982ceb0a941af64baec01ebc6b0cb0689
Angel Gustavo Pacheco Meza <gustavopacheco3005@gmail.com>
2026-09-13 00:27:07 -0700

    Segun yo aqui deberia de jalar, primera prueba

--- practica_1_Hola_Mundo/app/src/main/java/com/example/practica_1_hola_mundo/MainActivity.kt
+++ practica_1_Hola_Mundo/app/src/main/java/com/example/practica_1_hola_mundo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.practica_1_hola_mundo
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val texto = findViewById<EditText>(R.id.entradaTexto)
+        val boton = findViewById<Button>(R.id.botonSaludar)
+        val verMensaje = findViewById<TextView>(R.id.textView)
+
+        boton.setOnClickListener(){
+            val textIngresado = texto.text.toString()
+            verMensaje.text = "Que dice el viejo bien machin $textIngresado, si no escribiste un nombre, regresate a escribir un nombre, que esto es un saludo viejo"
+        }
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
