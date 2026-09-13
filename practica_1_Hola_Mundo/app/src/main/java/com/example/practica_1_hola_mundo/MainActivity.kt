53275fe439a276131714ffcda04e6528d6c63f93
Angel Gustavo Pacheco Meza <gustavopacheco3005@gmail.com>
2026-09-13 12:05:28 -0700

    Ahora si ya jala

--- practica_1_Hola_Mundo/app/src/main/java/com/example/practica_1_hola_mundo/MainActivity.kt
+++ practica_1_Hola_Mundo/app/src/main/java/com/example/practica_1_hola_mundo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.practica_1_hola_mundo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -10,22 +11,27 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val texto = findViewById<EditText>(R.id.entradaTexto)
-        val boton = findViewById<Button>(R.id.botonSaludar)
-        val verMensaje = findViewById<TextView>(R.id.textView)
-
-        boton.setOnClickListener(){
-            val textIngresado = texto.text.toString()
-            verMensaje.text = "Que dice el viejo bien machin $textIngresado, si no escribiste un nombre, regresate a escribir un nombre, que esto es un saludo viejo"
-        }
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val texto = findViewById<EditText>(R.id.entradaTexto)
+        val boton = findViewById<Button>(R.id.botonSaludar)
+        val verMensaje = findViewById<TextView>(R.id.textView)
+
+        boton.setOnClickListener() {
+            val textIngresado = texto.text.toString()
+            if (textIngresado.isEmpty()) {
+                verMensaje.text="Escribe algo viejo"
+            } else {
+                verMensaje.text = "Que dice el viejo bien machin $textIngresado, si no escribiste un nombre, regresate a escribir un nombre, que esto es un saludo viejo"
+            }
         }
     }
 }
