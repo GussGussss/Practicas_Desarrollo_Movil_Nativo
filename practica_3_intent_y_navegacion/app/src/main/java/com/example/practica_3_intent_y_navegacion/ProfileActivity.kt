899e5e1a76832df0bfa7751a5c6175358344d474
Angel Gustavo Pacheco Meza <gustavopacheco3005@gmail.com>
2026-09-20 18:59:31 -0700

    Practica terminado, nomas falta ejecutarla xD

--- practica_3_intent_y_navegacion/app/src/main/java/com/example/practica_3_intent_y_navegacion/ProfileActivity.kt
+++ practica_3_intent_y_navegacion/app/src/main/java/com/example/practica_3_intent_y_navegacion/ProfileActivity.kt
@@ -1,4 +1,23 @@
 package com.example.practica_3_intent_y_navegacion
 
-class ProfileActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ProfileActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //traemos los valores de Intent
+        val nombre = intent.getStringExtra("EXTRA_NOMBRE") ?: "Sin Nombre"
+        val correo = intent.getStringExtra("EXTRA_CORREO") ?: "Sin Correo"
+
+        setContent {
+            ProfileScreen(
+                nombre = nombre,
+                correo = correo,
+                onBackClick = {finish()}
+            )
+        }
+    }
 }
